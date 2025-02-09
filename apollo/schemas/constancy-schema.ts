3da40cb438f215af7137d7973d5798445d27e7e1
Slisandro <salvareschilisandro0@gmail.com>
2025-02-09 18:16:04 -0300

    change getAllConstancies schema

--- apollo/schemas/constancy-schema.ts
+++ apollo/schemas/constancy-schema.ts
@@ -4,15 +4,43 @@ const typeDefs = gql`
   type Constancy {
     id: ID!
     userId: String!
+    user: User
     courseId: String!
+    course: Course
     clientId: String! 
+    client: Client
     startDate: String!
     endDate: String!
     entityType: String!
   }
 
+  type Course {
+    id: ID!
+    name: String!
+    duration: Int!
+    entityType: String!
+  }
+
+  type Client {
+    id: ID!
+    name: String!
+    representative: String!
+    code: String!
+    entityType: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    role: String!
+    entityType: String!
+  }
+
   type Query {
     constanciesCount: Int
+    getAllConstancies: [Constancy!]
     getConstanciesByUser(userId: ID!): [Constancy!]
     getConstanciesByCourse(courseId: ID!): [Constancy!] 
     getConstanciesByClient(clientId: ID!): [Constancy!]
